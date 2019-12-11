29321ac8e0a28522b7b6109064ba1b2d8d7b9edf
Romy Lynch <romylynch@gmail.com>
2019-12-11 11:33:19 +0000

    new changes, fake records

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+
+        if @article.save
+            redirect_to @article, notice: 'Article was successfully created!'
+        else 
+            render :new
+        end
+    end
+
+    def edit
+        @article = Article.find(params[:id])
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            redirect_to @article, notice: 'Article was successfully updated!'
+        else 
+            render :edit
+        end
+    end
+
+    private
+
+    def article_params
+        params.required(:article).permit(:title, :content)
+    end
+    
+end
